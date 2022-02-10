4fe161921bc6833b62ad5a6c6be4c12291240d63
kksarkar <56276650+kksarkar@users.noreply.github.com>
2022-02-10 20:02:12 +0530

    pushing

--- spring-boot-hello-world-example/src/main/java/com/javatpoint/controller/HelloWorldController.java
+++ spring-boot-hello-world-example/src/main/java/com/javatpoint/controller/HelloWorldController.java
@@ -1,12 +1,41 @@
 package com.javatpoint.controller;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController 
 {
-@RequestMapping("/")
-public String hello() 
-{
-return "Hello javaTpoint";
-}
+	@RequestMapping("/")
+	public String hello() 
+	{
+		int arr[][] = {
+		{2,100},
+		{5,50}
+				};
+		
+		int prev = arr[0][0];
+		int prevVal = arr[0][1];
+		int cnt =arr[0][0];
+		
+		Map<Integer, Integer> map = new HashMap<>();
+		int cnt1 =1;
+		while(cnt!=12)
+		{
+			cnt++;
+			if(cnt == arr[cnt1][0])
+			{
+				
+			}
+			else {
+				arr[cnt1][0] = prev;
+				
+			}
+			
+		}
+		return "Hello javaTpoint";
+	
+	
+	}
 }
